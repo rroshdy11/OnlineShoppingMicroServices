8c9be2be6b4389285a8a99dcbb216f3a1f1c32a5
Mshafei2010 <mshafei2010@gmail.com>
2023-04-19 06:07:16 +0200

    Shipping Company

---
+++ ShippingCompanyServices/ShippingCompanyServices/Models/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShippingCompanyServices.Models
+{
+    public class Context:DbContext
+    {
+        public Context() { }
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+        public DbSet<ShippingCompany> ShippingCompany { get; set; }
+    }
+}
