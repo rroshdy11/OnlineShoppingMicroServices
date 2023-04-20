b7e53fe2f270bc1b8e3f13fa54bc7033d3fb0941
Mshafei2010 <mshafei2010@gmail.com>
2023-04-20 11:31:45 +0200

    Selling Company & Logs

---
+++ SellingCompany/src/main/java/SellingCompany/SellingCompany.java
@@ -0,0 +1,22 @@
+package SellingCompany;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "sellingcompany")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class SellingCompany {
+    @Id
+    private String name;
+    private String password;
+    private String adminName;
+
+}
+
