2c590b1f9c348a1e9ea814e599859ab18b53df51
Reda Roshdy <rroshdy75@gmail.com>
2023-04-20 22:52:23 +0200

    Session Auth and Stateless Admin

--- AdminServices/src/main/java/Admin/Admin.java
+++ AdminServices/src/main/java/Admin/Admin.java
@@ -1,6 +1,7 @@
 package Admin;
 
 import jakarta.ejb.Stateful;
+import jakarta.ejb.Stateless;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +14,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter
 @Getter
-@Stateful
+@Stateless
 public class Admin {
     @Id
     private String username;
